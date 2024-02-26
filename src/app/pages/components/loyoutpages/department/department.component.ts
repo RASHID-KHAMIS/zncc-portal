1185c635f693418ce42db884f96992f4fba89dc8
RASHID-KHAMIS <rashidkhamis168@gmail.com>
2024-02-26 13:33:22 +0300

    26-02-2024 13:33

--- src/app/pages/components/loyoutpages/department/department.component.ts
+++ src/app/pages/components/loyoutpages/department/department.component.ts
@@ -1,10 +1,172 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DepartmentService } from 'src/app/pages/services/department.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
   styleUrls: ['./department.component.css']
 })
-export class DepartmentComponent {
+export class DepartmentComponent implements OnInit{
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['No', 'departmentCode', 'departmentName','action'];
+  loading: boolean = true;
+  @ViewChild('distributionDialog') distributionDialog!: TemplateRef<any>;
+  @ViewChild('distributionDialog2') distributionDialog2!: TemplateRef<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  departmentForm!:FormGroup;
+  departmentEditForm!:FormGroup;
+  constructor(private router:Router,
+    private route:ActivatedRoute,
+    private dialog:MatDialog,
+    private departmentService:DepartmentService){}
+  ngOnInit(): void {
+    this.fetchAllDepartment()
+    this.configureForm();
+    this.configureEditForm();
+  
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  fetchAllDepartment(){
+    this.departmentService.getAllDepartment().subscribe((resp:any)=>{
+      this.dataSource = new MatTableDataSource(resp);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.loading = false;
+    })
+  }
+
+  configureForm(){
+    this.departmentForm = new FormGroup({
+      departmentCode:new FormControl(null,Validators.required),
+      departmentName:new FormControl(null,Validators.required),
+    })
+  }
+
+  configureEditForm(){
+    this.departmentEditForm = new FormGroup({
+      departmentId:new FormControl(null),
+      departmentCode:new FormControl(null,Validators.required),
+      departmentName:new FormControl(null,Validators.required),
+    })
+  }
+
+  onSave(){
+    const values = this.departmentForm.value;
+    this.departmentService.addDepartment(values).subscribe((resp:any)=>{
+    
+      this.alert();
+      this.reload();
+    })
+  }
+
+  onEdit(){
+    const id = this.departmentEditForm.value.departmentId;
+    const values = this.departmentEditForm.value;
+    this.departmentService.editDepartment(id,values).subscribe((resp:any)=>{
+      this.alert2();
+      this.reload();
+    })
+  }
+
+  openDialog() {
+
+    let dialogRef = this.dialog.open(this.distributionDialog, {
+      width: '650px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        if (result !== 'no') {
+          const enabled = "Y"
+
+        } else if (result === 'no') {
+        }
+      }
+    })
+  }
+
+  openDialog2(row:any) {
+    this.departmentEditForm = new FormGroup({
+      departmentId:new FormControl(row.departmentId),
+      departmentCode:new FormControl(row.departmentCode),
+      departmentName:new FormControl(row.departmentName),
+    })
+    let dialogRef = this.dialog.open(this.distributionDialog2, {
+      width: '650px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        if (result !== 'no') {
+          const enabled = "Y"
+
+        } else if (result === 'no') {
+        }
+      }
+    })
+  }
+
+  reload(){
+    this.router.navigateByUrl('',{skipLocationChange:true}).then(()=>{
+      this.router.navigate(['department'])
+    })
+  }
+
+  alert(){
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Department Added Successfully'
+    })
+  }
+
+
+  alert2(){
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Department Edited Successfully'
+    })
+  }
+
+
+
 
 }
