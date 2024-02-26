b51c3f6563d5a9396dbcf03bb401fce739bbd263
RASHID-KHAMIS <rashidkhamis168@gmail.com>
2024-02-26 17:53:34 +0300

    26-02-2024 17:53

--- src/app/pages/components/layout/layout.component.ts
+++ src/app/pages/components/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-layout',
@@ -28,8 +29,34 @@ export class LayoutComponent {
     // }
   }
 
-  logout() {
-    localStorage.clear()
-    this.router.navigate(["/"])
+  // Onlogout() {
+  //   localStorage.clear()
+  //   this.router.navigate(["/"])
+  // }
+
+  onLogOut(){
+    localStorage.clear();
+    this.router.navigateByUrl("/");
+    this.alert();
   }
+
+  alert(){
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'error',
+      title: 'Signed Out Successfully'
+    })
+  }
+
 }
